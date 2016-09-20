ccda1736eaa1468e0e466de174c29ebc3cb5c331
Ne1c <codeneic@gmail.com>
2016-09-21 00:15:54 +0300

    Some update

--- annotations/src/main/java/com/ne1c/rainbowmvp/annotaions/StorableProcessor.java
+++ annotations/src/main/java/com/ne1c/rainbowmvp/annotaions/StorableProcessor.java
@@ -7,13 +7,21 @@ import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.TypeElement;
 
-@SupportedAnnotationTypes({"*"})
+@SupportedAnnotationTypes({"com.ne1c.rainbowmvp.annotaions.StorableView"})
 @SupportedSourceVersion(SourceVersion.RELEASE_6)
 public class StorableProcessor extends AbstractProcessor {
+    public StorableProcessor() {}
+
     @Override
-    public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
+    public boolean process(Set<? extends TypeElement> set, RoundEnvironment env) {
+        for (Element e : env.getElementsAnnotatedWith(StorableView.class)) {
+            if (e.getKind() == ElementKind.INTERFACE) {
+            }
+        }
         return false;
     }
 }
