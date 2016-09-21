70871c9ae73a4d16c99fa785bbd77dea4a2f3098
Ne1c <codeneic@gmail.com>
2016-09-21 12:45:58 +0300

    Minor update

--- rainbowmvp-annotations/src/main/java/com/ne1c/rainbowmvp/processor/StorableProcessor.java
+++ rainbowmvp-annotations/src/main/java/com/ne1c/rainbowmvp/processor/StorableProcessor.java
@@ -1,20 +1,23 @@
-package com.ne1c.rainbowmvp.annotaions;
+package com.ne1c.rainbowmvp.processor;
 
+import com.google.auto.service.AutoService;
+import com.ne1c.rainbowmvp.annotaions.StorableView;
+
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
-import javax.annotation.processing.SupportedAnnotationTypes;
-import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.TypeElement;
 
-@SupportedAnnotationTypes({"com.ne1c.rainbowmvp.annotaions.StorableView"})
-@SupportedSourceVersion(SourceVersion.RELEASE_6)
+@AutoService(Processor.class)
 public class StorableProcessor extends AbstractProcessor {
-    public StorableProcessor() {}
+    public StorableProcessor() {
+    }
 
     @Override
     public boolean process(Set<? extends TypeElement> set, RoundEnvironment env) {
@@ -23,5 +26,17 @@ public class StorableProcessor extends AbstractProcessor {
             }
         }
         return false;
+    }
+
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        Set<String> annotations = new LinkedHashSet<>();
+        annotations.add(StorableView.class.getCanonicalName());
+        return annotations;
+    }
+
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        return SourceVersion.latestSupported();
     }
 }
