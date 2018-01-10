36693e7b0a251ae3fd75990674d88ac05ed687bb
n.kucheriaviy <n.kucheriaviy@smartweb.com.ua>
2018-01-10 16:07:43 +0200

    Add generator class

--- rainbowmvp-processor/src/main/java/com/ne1c/rainbowmvp/processor/PresenterTagProcessor.java
+++ rainbowmvp-processor/src/main/java/com/ne1c/rainbowmvp/processor/PresenterTagProcessor.java
@@ -1,39 +1,62 @@
 package com.ne1c.rainbowmvp.processor;
 
 import com.ne1c.rainbowmvp.annotaions.PresenterTag;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.TypeSpec;
 
+import java.io.IOException;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Filer;
 import javax.annotation.processing.Messager;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
 
+// Run for debug: ./gradlew --no-daemon -Dorg.gradle.debug=true :sample:clean :sample:compileDebugJavaWithJavac
+
 public class PresenterTagProcessor extends AbstractProcessor {
     private Messager messager;
+    private Filer filer;
 
     @Override
     public synchronized void init(ProcessingEnvironment processingEnvironment) {
         super.init(processingEnvironment);
 
         messager = processingEnvironment.getMessager();
+        filer = processingEnvironment.getFiler();
     }
 
     @Override
-    public boolean process(Set<? extends TypeElement> set, RoundEnvironment env) {
-        for (Element e : env.getElementsAnnotatedWith(PresenterTag.class)) {
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        for (Element e : roundEnv.getElementsAnnotatedWith(PresenterTag.class)) {
             if (e.getKind() != ElementKind.CLASS) {
                 messager.printMessage(Diagnostic.Kind.ERROR, "Can be applied only to class.");
                 return true;
             }
+
+            TypeSpec testClass = TypeSpec.classBuilder("MyTestClass")
+                    .addModifiers(Modifier.PUBLIC)
+                    .build();
+
+            JavaFile testJavaFile = JavaFile.builder(e.getEnclosingElement().toString(), testClass)
+                    .build();
+
+            try {
+                testJavaFile.writeTo(filer);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
         }
-        return false;
+
+        return true;
     }
 
     @Override
